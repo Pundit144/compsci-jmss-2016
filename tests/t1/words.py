b29443aa0338665e39d06f84cde1d205f69d076d
Neel Upasani <upa0001@jmss.vic.edu.au>
2016-02-29 14:49:03 +1100

    tried to complete task

--- tests/t1/words.py
+++ tests/t1/words.py
@@ -1,2 +1,14 @@
 # Write a program to read in words from the keyboard one at a time until the word "quit" is typed.
 # Store them in a list then print them alphabetically
+
+i = 0
+string = ""
+
+while i<1:
+    word = input ("you can type now")
+    string += word
+
+    print ("ahhhh, I don't know what to do")
+
+    if word == "quit":
+        i = 2
